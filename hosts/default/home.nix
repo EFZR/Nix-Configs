7b84f9ccab1108ee64526e0198556b839d3fad5b
Emerson Fernando Zapata Rivas <emerson.zapatarivas@gmail.com>
2024-10-13 20:24:51 -0600

    Update home.nix with zsh configs

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -79,4 +79,21 @@
     userName  = "efzr";
     userEmail = "emerson.zapatarivas@gmail.com";
   };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      ll = "ls -l";
+      update = "sudo nixos-rebuild switch";
+    };
+
+    history = {
+      size = 10000;
+      path = "${config.xdg.dataHome}/zsh/history";
+    };
+  };
 }

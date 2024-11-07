1a84c6ee62202b945af5ec45180cf9fb15ddf303
efzr <emerson.zapatarivas@gmail.com>
2024-11-07 04:30:32 +0000

    Add a new host to work from work

--- flake.nix
+++ flake.nix
@@ -1,44 +1,54 @@
 {
   description = "A very basic flake";
-  
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
     home-manager = {
-	url = "github:nix-community/home-manager";
-	inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, nixos-wsl, home-manager, ... }@inputs: {
-    nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-	specialArgs = {inherit inputs;};
-        modules = [
-          ./hosts/default/configuration.nix
-          nixos-wsl.nixosModules.default
-          {
-            system.stateVersion = "24.05";
-            wsl.enable = true;
-          }
-	  home-manager.nixosModules.default
-        ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixos-wsl,
+      home-manager,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations = {
+        # Personal configuration
+        nixos = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/default/configuration.nix
+            nixos-wsl.nixosModules.default
+            home-manager.nixosModules.default
+            {
+              system.stateVersion = "24.05";
+              wsl.enable = true;
+            }
+          ];
+        };
+
+        # Work configuration
+        work = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/work/configuration.nix
+            nixos-wsl.nixosModules.default
+            home-manager.nixosModules.default
+            {
+              system.stateVersion = "24.05";
+              wsl.enable = true;
+				    }
+          ];
+        };
       };
     };
-
-    #	workMachine = {
-    #	      nixos = nixpkgs.lib.nixosSystem {
-    #		system = "x86_64-linux";
-    #		modules = [
-    #		  ./hosts/workMachine/configuration.nix
-    #		  nixos-wsl.nixosModules.default
-    #		  {
-    #		    system.stateVersion = "24.05";
-    #		    wsl.enable = true;
-    #		  }
-    #		];
-    #	      };
-    #	    };
-  };  
 }
